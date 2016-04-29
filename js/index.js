c1946876a7f6ff385d4fa20264a0e53acf8fb1eb
RainyRuRu <believeu123@gmail.com>
2016-04-29 18:05:05 +0800

    oh

--- js/index.js
+++ js/index.js
@@ -2,15 +2,32 @@ $(window).resize(function() {
     load();
 })
 
-var nowIndex = 0;
-var list = ["#home","#info"];
-
 $(window).scroll(function () {
-
     var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
     var scrollTop = $(window).scrollTop();
+    var homeButton = $("#homeButton");
 
+    if (windowWidth < homeButton.width() * 2) {
+        homeButton.show();
+        return;
+    }
+
+    if (scrollTop > windowHeight * 0.8) {
+        homeButton.hide();
+    } else {
+        homeButton.show();
+    }
 });
+
+function scrollToPage(type) {
+    var marginTopStr = $(type).css("margin-top");
+    var marginTop = marginTopStr.substring(0, marginTopStr.length-2);
+
+    $('html, body').animate({
+        scrollTop: $(type).offset().top - marginTop
+    }, 2000);
+}
 
 function load() {
     var windowHeight = $(window).height();
@@ -23,7 +40,19 @@ function load() {
         $("#homeBackground").css('background-position', 'center center');
         $("#home").css('height', windowHeight+"px");
     }
+}
 
-    console.log(windowHeight);
-    console.log(windowWidth);
+var experienceList = {
+    "test": ["kkk", "kkbox"],
+    "test2": ["value2"]
+};
+
+
+function type(info) {
+    $("#experienceText").typed('reset');
+    $("#experienceText").typed({
+        strings: experienceList[info],
+        contentType: 'html',
+        typeSpeed: 2
+     });
 }
